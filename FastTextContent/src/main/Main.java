34028d8ab728c68a15c91f232b1892d3b700419a
nlrarojas <nlrarojas@hotmail.com>
2017-05-03 12:20:52 -0600

    Se modifica el proyecto antiguo por una nueva versión

--- FastTextContent/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import view.PrincipalWindow;
-
-public class Main{
-	public static void main(String[] args) {
-		new PrincipalWindow();
-	}
-}	
--- FastTextContent/src/util/Utility.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public interface Utility {
-	public static final int WIDTH_RESOLUTION = 1360;
-	public static final int HEIGHT_RESOLUTION = 768;
-	public static final String URL_ADRESS = "http://www.google.com/search?q=";
-}
